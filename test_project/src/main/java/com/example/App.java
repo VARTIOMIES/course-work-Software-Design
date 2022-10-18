c0c8b8cdf0b5f2978f493aeeb7730bec44f61251
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-10-18 13:41:28 +0300

    add test_project

---
+++ test_project/src/main/java/com/example/App.java
@@ -0,0 +1,86 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.example.FeedElement;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        File file = new File("./data/road_conditions.json");
+        Scanner myReader = new Scanner(file);
+        StringBuilder str = new StringBuilder();
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            str.append(data);
+        }
+        myReader.close();
+
+        JSONObject obj = new JSONObject(str.toString());
+        JSONArray weather_data = obj.getJSONArray("weatherData");
+        VBox feed = new VBox(10);
+        feed.setId("box");
+        for (Object datapoint_obj : weather_data) {
+
+            JSONObject location = (JSONObject) datapoint_obj;
+
+            JSONArray raodConditions = location.getJSONArray("roadConditions");
+
+            // loop one location
+            for (Object location_datapoint : raodConditions) {
+
+                FeedElement feed_element = new FeedElement();
+
+                JSONObject point = (JSONObject) location_datapoint;
+                feed_element.setTitle("this is title");
+                feed_element.addAllInfo(point.getString("type"), point.getString("forecastName"), "temperature: ",
+                        point.getString("temperature"));
+                feed.getChildren().add(feed_element.getObject());
+            }
+
+        }
+        ScrollPane scrollable = new ScrollPane();
+        scrollable.setId("scroll");
+        scrollable.setContent(feed);
+        scene = new Scene(scrollable, 640, 480);
+        scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ test_project/src/main/java/com/example/FeedElement.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class FeedElement {
+    private Text title_obj;
+    private VBox feed_obj;
+    private VBox info_box;
+
+    public FeedElement() {
+        feed_obj = new VBox();
+        feed_obj.setId("feed-element");
+        info_box = new VBox();
+        title_obj = new Text();
+        title_obj.setId("title");
+    }
+
+    public void addInfo(String info){
+        info_box.getChildren().add(new Text(info));
+    }
+
+    public void addAllInfo(String... list){
+        for(String text : list){
+            Text t = new Text(text);
+            info_box.getChildren().add(t);
+        }
+    }
+
+    public void setTitle(String title){
+        title_obj.setText(title);
+    }
+
+    public VBox getObject(){
+        feed_obj.getChildren().add(title_obj);
+        feed_obj.getChildren().add(info_box);
+
+        return feed_obj;
+    }
+}
