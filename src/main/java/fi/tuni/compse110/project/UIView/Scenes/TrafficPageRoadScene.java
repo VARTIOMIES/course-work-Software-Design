92952a0e3cc4f14749607270a7850aecab9447e4
Onni Merilä <onni.merila@tuni.fi>
2022-12-01 22:28:47 +0200

    Refactor TrafficPageScene to work like TrafficPageRoadScene, add empty graph info textbox and style

--- src/main/java/fi/tuni/compse110/project/UIView/Scenes/TrafficPageRoadScene.java
+++ src/main/java/fi/tuni/compse110/project/UIView/Scenes/TrafficPageRoadScene.java
@@ -7,8 +7,10 @@ import fi.tuni.compse110.project.Graph.GraphProvider;
 import fi.tuni.compse110.project.UIView.UIController;
 import fi.tuni.compse110.project.UIView.components.SidePanel;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -17,6 +19,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import java.io.IOException;
 import java.util.*;
+
+import javafx.scene.text.TextAlignment;
 import org.jfree.chart.fx.ChartViewer;
 import fi.tuni.compse110.project.UIView.components.Feed;
 
@@ -34,14 +38,17 @@ public class TrafficPageRoadScene extends Scene {
     HBox horizontalrootElementContainer;
     Pane graph;
     UIController controller;
-    int roadNumber;
+    private int roadNumber;
     private ArrayList<Double> testCoords;
     private SidePanel sidepanel;
 
     private List<RoadCondition> specificRCData;
     private ArrayList<UIController.Plottable> wantedData;
 
-    public TrafficPageRoadScene(ScrollPane root, double v, double v1, UIController controller) {
+    public TrafficPageRoadScene(ScrollPane root,
+                                double v,
+                                double v1,
+                                UIController controller) {
         super(root, v, v1);
         this.controller = controller;
 
@@ -49,37 +56,62 @@ public class TrafficPageRoadScene extends Scene {
 
         mainContent = new VBox(20);
 
-        feed_window = new VBox(20);
-
         vLayout = new VBox(20);
-        // taskFeed = new Feed(new HashMap<>());
+        vLayout.setMinWidth(1024);
+        vLayout.setAlignment(Pos.CENTER);
+        vLayout.setId("background");
+
         horizontalrootElementContainer = new HBox();
-        sidepanel = new SidePanel(20,
-                this.controller,
-                UIController.CurrentSceneEnum.TRAFFIC_SCENE_ROAD
-        );
-        graph = new Pane();
-        graph.setId("graph");
+        horizontalrootElementContainer.setId("row");
 
         specificRCData = new ArrayList<>();
 
-        initFeed();
+        roadNumber = 1;
 
+
+        //createContent();
+
+        Button backButton = new Button("<- back to menu");
+        backButton.setPrefSize(120, 40);
+        backButton.setOnAction(event -> backToMenuClickHandle());
+
+        vLayout.getChildren().add(backButton);
+
+
+        graph = new Pane();
+        graph.setId("graph");
         wantedData = new ArrayList<>();
         wantedData.add(UIController.Plottable.ROAD_TEMPERATURE);
         initChartViewer("");
 
+        initFeed();
 
-        createContent();
+
+        mainContent.getChildren().addAll(graph, feed_window);
+
+        Region filler = new Region();
+        filler.setPrefWidth(50);
+        sidepanel = new SidePanel(20,
+                this.controller,
+                UIController.CurrentSceneEnum.TRAFFIC_SCENE_ROAD
+        );
+
+        horizontalrootElementContainer.getChildren().addAll(mainContent, filler, sidepanel);
+
+        vLayout.getChildren().addAll(horizontalrootElementContainer);
+
+        // Possible nullPointerException throwing from .toExternalForm()
+        this.getStylesheets().add(TrafficPageRoadScene.class.getResource("/stylesheet.css").toExternalForm());
+
         root.setContent(vLayout);
-        roadNumber = 1;
+
+
 
     }
 
     public void getDataFromApi(){
         int roadNumber = 1;
         int sectionArrayListIndex = 1;
-        String titleForChart = "helo";
         try{
             specificRCData = RoadDataProvider.getSpecificSectionRoadCondition(
                     roadNumber,
@@ -97,106 +129,19 @@ public class TrafficPageRoadScene extends Scene {
         if (info_text.isEmpty()){
             info_text = "Nothing to show yet :(";
         }
-        Text emptytext = new Text(info_text);
-        graph.getChildren().setAll(emptytext);
         graph.setPrefSize(634,500);
+
+        Label emptytext = new Label(info_text);
+        emptytext.setAlignment(Pos.CENTER);
+        emptytext.minWidthProperty().bind(graph.widthProperty());
+        emptytext.minHeightProperty().bind(graph.heightProperty());
+
+        graph.getChildren().setAll(emptytext);
     }
 
-    public void makeNewChartViewer(ArrayList<Double> coords,
-                                   ArrayList<UIController.Plottable> selectedPlottables){
-        UIController.Plottable wantedData = UIController.Plottable.ROAD_TEMPERATURE;
-        int roadNumber = 1;
-        int sectionArrayListIndex = 1;
-        String titleForChart = "helo";
-        try {
-            getDataFromApi();
-            ChartViewer dataChartViewer = GraphProvider.getRoadConditionChart(
-                    634,
-                    500,
-                    specificRCData,
-                    wantedData,
-                    titleForChart
-            );
-            graph.getChildren().setAll(dataChartViewer);
-        }
-        catch (Exception e){ // If there occurs any errors while creating the chart
-            // from API data, creates a hardcoded chart to act as a placeholder
-            //System.out.println("error");
-            initChartViewer("Error: code 69");
-        }
-
-    }
-
-    public void createContent() {
-        vLayout.getChildren().clear();
-        horizontalrootElementContainer.getChildren().clear();
-        graph.getChildren().clear();
-
-        //ArrayList<Double> coords = new ArrayList<>(Arrays.asList(25.72088, 62.24147, 25.8, 62.3));
-
-        // vertical layout
-
-        vLayout.setMinWidth(1024);
-        vLayout.setAlignment(Pos.CENTER);
-        vLayout.setId("background");
-
-        Button backButton = new Button("<- back to menu");
-        backButton.setPrefSize(120, 40);
-        backButton.setOnAction(event -> backToMenuClickHandle());
-
-        vLayout.getChildren().add(backButton);
-        
-        horizontalrootElementContainer.setId("row");
-
-
-        // load roadcondition data for the feed
-
-        Region filler = new Region();
-        filler.setPrefWidth(50);
-
-
-        mainContent.getChildren().addAll(graph, feed_window);
-        horizontalrootElementContainer.getChildren().addAll(mainContent, filler, sidepanel);
-
-        vLayout.getChildren().addAll(horizontalrootElementContainer);
-
-        // Possible nullPointerException throwing from .toExternalForm()
-        this.getStylesheets().add(TrafficPageRoadScene.class.getResource("/stylesheet.css").toExternalForm());
-
-    }
-
-    private void searchButtonClicked(){
-        getDataFromApi();
-        refreshWithNewData();
-    }
-
-    private void populateFeed(){
-        Map<ArrayList<String>, ArrayList<String>> task_list = new HashMap<>();
-        if (!specificRCData.isEmpty()) {
-            for (var condition : specificRCData) {
-                task_list.put(new ArrayList<String>(Arrays.asList("Section " + condition.getSection())),
-                        new ArrayList<String>(Arrays.asList(
-                                "Precipitation: " + condition.getPrecipitationCondition(),
-                                "Overall road condition: " + condition.getOverallRoadCondition(),
-                                "Road condition: " + condition.getRoadCondition()
-                        )));
-            }
-        } else {
-            task_list.put(new ArrayList<String>(Arrays.asList("no data for this road")),
-                    new ArrayList<String>(Arrays.asList(
-                            "")));
-        }
-        taskFeed = new Feed(task_list);
-    }
-
-    private void refreshWithNewData(){
-
-        makeNewChartViewer(testCoords,wantedData);
-        populateFeed();
-        feed_window.getChildren().set(1,taskFeed.getElement());
-    }
-
-
+    /**
+     * Assumes that this.roadNumber,this.controller are already initiated
+     */
     private void initFeed(){
 
         // feed navigation bar top
@@ -208,7 +153,6 @@ public class TrafficPageRoadScene extends Scene {
             if (roadNumber - 1 >= 0){
                 roadNumber = roadNumber - 1;
                 refreshWithNewData();
-                controller.refresh();
             }
 
         });
@@ -218,7 +162,6 @@ public class TrafficPageRoadScene extends Scene {
         next_road.setOnAction(event -> {
             roadNumber = roadNumber + 1;
             refreshWithNewData();
-            controller.refresh();
         });
 
         Text road_number_text = new Text("Road " + roadNumber);
@@ -247,10 +190,66 @@ public class TrafficPageRoadScene extends Scene {
 
         populateFeed();
 
+        feed_window = new VBox(20);
         feed_window.setId("feed-window");
         feed_window.setAlignment(Pos.TOP_RIGHT);
         feed_window.getChildren().addAll(feed_navigation_bar, taskFeed.getElement(), feed_timerange_bar);
 
+    }
+
+
+    public void makeNewChartViewer(ArrayList<Double> coords,
+                                   ArrayList<UIController.Plottable> selectedPlottables){
+        UIController.Plottable wantedData = UIController.Plottable.ROAD_TEMPERATURE;
+        int roadNumber = 1;
+        int sectionArrayListIndex = 1;
+        String titleForChart = "helo";
+        try {
+            getDataFromApi();
+            ChartViewer dataChartViewer = GraphProvider.getRoadConditionChart(
+                    634,
+                    500,
+                    specificRCData,
+                    wantedData,
+                    titleForChart
+            );
+            graph.getChildren().setAll(dataChartViewer);
+        }
+        catch (Exception e){ // If there occurs any errors while creating the chart
+            // from API data, creates a hardcoded chart to act as a placeholder
+            //System.out.println("error");
+            initChartViewer("Error: code 69");
+        }
+
+    }
+
+
+
+    private void populateFeed(){
+        Map<ArrayList<String>, ArrayList<String>> task_list = new HashMap<>();
+        if (!specificRCData.isEmpty()) {
+            for (var condition : specificRCData) {
+                task_list.put(new ArrayList<String>(Arrays.asList("Section " + condition.getSection())),
+                        new ArrayList<String>(Arrays.asList(
+                                "Precipitation: " + condition.getPrecipitationCondition(),
+                                "Overall road condition: " + condition.getOverallRoadCondition(),
+                                "Road condition: " + condition.getRoadCondition()
+                        )));
+            }
+        } else {
+            task_list.put(new ArrayList<String>(Arrays.asList("no data for this road")),
+                    new ArrayList<String>(Arrays.asList(
+                            "")));
+        }
+        taskFeed = new Feed(task_list);
+    }
+
+
+    private void refreshWithNewData(){
+
+        makeNewChartViewer(testCoords,wantedData);
+        populateFeed();
+        feed_window.getChildren().set(1,taskFeed.getElement());
     }
 
 
@@ -261,5 +260,11 @@ public class TrafficPageRoadScene extends Scene {
         // Stuff happening after the "back to menu" button click
         controller.fromAnyPageToMenu();
     }
+
+    private void handleSearchButtonClick(){
+        getDataFromApi();
+        refreshWithNewData();
+    }
+
 
 }
--- src/main/java/fi/tuni/compse110/project/UIView/Scenes/TrafficPageScene.java
+++ src/main/java/fi/tuni/compse110/project/UIView/Scenes/TrafficPageScene.java
@@ -9,6 +9,7 @@ import fi.tuni.compse110.project.UIView.components.SidePanel;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -41,39 +42,162 @@ public class TrafficPageScene extends Scene{
     private Pane graph;
     private VBox feed_window;
 
-
+    private int roadNumber;
     private ArrayList<Double> coords;
     private UIController controller;
 
-    public TrafficPageScene(ScrollPane root, double v, double v1,UIController controller) {
+    private List<RoadCondition> specificRCData;
+    private ArrayList<UIController.Plottable> wantedData;
+
+
+    public TrafficPageScene(ScrollPane root,
+                            double v,
+                            double v1,
+                            UIController controller) {
         super(root,v,v1);
         this.controller = controller;
+
 
         coords = new ArrayList<>(Arrays.asList(25.72088, 62.24147, 25.8, 62.3));
 
         rootVerticalContainer = new VBox(20);
+        rootVerticalContainer.setMinWidth(1024);
+        rootVerticalContainer.setAlignment(Pos.CENTER);
+        rootVerticalContainer.setId("background");
         taskFeed = new Feed(new HashMap<>());
+
         horizontalrootElementContainer = new HBox();
+        horizontalrootElementContainer.setId("row");
+
+        specificRCData = new ArrayList<>();
+        roadNumber = 1;
 
         mainContent = new VBox(20);
         sidepanel = new SidePanel(20,
                 this.controller,
                 UIController.CurrentSceneEnum.TRAFFIC_SCENE
         );
-        graph = new Pane();
+
 
         feed_window = new VBox(20);
 
-        horizontalrootElementContainer.setId("row");
+        graph = new Pane();
         graph.setId("graph");
 
         //initSidePanel();
         initChartViewer("");
         initFeed();
-        createContent();
+
+
+
+        Button backButton = new Button("<- back to menu");
+        backButton.setPrefSize(120,40);
+        backButton.setOnAction(event->backToMenuClickHandle());
+        rootVerticalContainer.getChildren().add(backButton);
+
+        filler = new Region();
+        filler.setPrefWidth(50);
+
+        mainContent.getChildren().addAll(graph, feed_window);
+
+        horizontalrootElementContainer.getChildren().addAll(mainContent,filler, sidepanel);
+
+        rootVerticalContainer.getChildren().addAll(horizontalrootElementContainer);
+
+
+        // Possible nullPointerException throwing from .toExternalForm()
+        this.getStylesheets().add(TrafficPageScene.class.getResource("/stylesheet.css").toExternalForm());
+
         root.setContent(rootVerticalContainer);
 
     }
+
+    public void getDataFromApi(){
+        int roadNumber = 1;
+        int sectionArrayListIndex = 1;
+        try{
+            specificRCData = RoadDataProvider.getSpecificSectionRoadCondition(
+                    roadNumber,
+                    sectionArrayListIndex,
+                    coords
+            );
+        }
+        catch (Exception e){
+            System.out.println("Error with getting the data from api");
+        }
+
+    }
+
+    public void initChartViewer(String info_text){
+
+        if (info_text.isEmpty()){
+            info_text = "Nothing to show yet :(";
+        }
+        graph.setPrefSize(634,500);
+
+        Label emptytext = new Label(info_text);
+        emptytext.setAlignment(Pos.CENTER);
+        emptytext.minWidthProperty().bind(graph.widthProperty());
+        emptytext.minHeightProperty().bind(graph.heightProperty());
+
+        graph.getChildren().setAll(emptytext);
+    }
+
+    private void initFeed(){
+
+        // feed navigation bar top
+        HBox feed_navigation_bar = new HBox();
+        Button previous_road = new Button("<--");
+        previous_road.setId("title");
+
+        previous_road.setOnAction(event -> {
+            if (roadNumber - 1 >= 0){
+                roadNumber = roadNumber - 1;
+                refreshWithNewData();
+            }
+
+        });
+
+        Button next_road = new Button("-->");
+        next_road.setId("title");
+        next_road.setOnAction(event -> {
+            roadNumber = roadNumber + 1;
+            refreshWithNewData();
+        });
+
+        Text road_number_text = new Text("Road " + roadNumber);
+        road_number_text.setId("title");
+        feed_navigation_bar.setSpacing(30);
+
+        Region fill_top = new Region();
+        fill_top.setPrefWidth(90);
+
+        feed_navigation_bar.getChildren().addAll(fill_top, previous_road, road_number_text, next_road);
+
+        // feed navigation bottom
+        HBox feed_timerange_bar = new HBox();
+
+        Button previus_timerange = new Button("<--");
+        previus_timerange.setId("title");
+        Button next_timerange = new Button("-->");
+        next_timerange.setId("title");
+        Text current_time_text = new Text("2h");
+        current_time_text.setId("title");
+
+        Region fill_bottom = new Region();
+        fill_bottom.setPrefWidth(100);
+        feed_timerange_bar.setSpacing(30);
+        feed_timerange_bar.getChildren().addAll(fill_bottom, previus_timerange, current_time_text, next_timerange);
+
+        populateFeed();
+
+        feed_window = new VBox(20);
+        feed_window.setId("feed-window");
+        feed_window.setAlignment(Pos.TOP_RIGHT);
+        feed_window.getChildren().addAll(feed_navigation_bar, taskFeed.getElement(), feed_timerange_bar);
+
+    }
+
     public void makeNewChartViewer(ArrayList<Double> coords,
                                    ArrayList<UIController.Plottable> selectedPlottables){
         UIController.Plottable wantedData = UIController.Plottable.ROAD_TEMPERATURE;
@@ -104,71 +228,24 @@ public class TrafficPageScene extends Scene{
 
     }
 
-    public void initChartViewer(String info_text){
-        /*
-            Here are some easily modifiable parameters to get different kinds of data,
-            These are useful especially for the begin-phase of the program
-         */
-        int roadNumber = 5;
-        int sectionArrayListIndex = 3;
-        UIController.Plottable wantedData = UIController.Plottable.ROAD_TEMPERATURE;
-        String titleForChart = "Road:" + roadNumber + "  Section:" + sectionArrayListIndex;
 
-        try {
-            List<RoadCondition> specificRCData = RoadDataProvider.getSpecificSectionRoadCondition(roadNumber,sectionArrayListIndex,this.coords);
-            ChartViewer dataChartViewer = GraphProvider.getRoadConditionChart(634,500,specificRCData, wantedData,titleForChart);
-            graph.getChildren().add(dataChartViewer);
+    private void populateFeed(){
+        Map<ArrayList<String>, ArrayList<String>> task_list = new HashMap<>();
+        if (!specificRCData.isEmpty()) {
+            for (var condition : specificRCData) {
+                task_list.put(new ArrayList<String>(Arrays.asList("Section " + condition.getSection())),
+                        new ArrayList<String>(Arrays.asList(
+                                "Precipitation: " + condition.getPrecipitationCondition(),
+                                "Overall road condition: " + condition.getOverallRoadCondition(),
+                                "Road condition: " + condition.getRoadCondition()
+                        )));
+            }
+        } else {
+            task_list.put(new ArrayList<String>(Arrays.asList("no data for this road")),
+                    new ArrayList<String>(Arrays.asList(
+                            "")));
         }
-        catch (Exception e){ // If there occurs any errors while creating the chart
-            // from API data, creates a hardcoded chart to act as a placeholder
-            //System.out.println("error");
-            ChartViewer testChartViewer = GraphProvider.getTestChart(500,400);
-            graph.getChildren().add(testChartViewer);
-        }
-        if (info_text.isEmpty()){
-            info_text = "Nothing to show yet :(";
-        }
-        Text emptytext = new Text(info_text);
-        graph.getChildren().setAll(emptytext);
-        graph.setPrefSize(634,500);
-    }
-
-    private void initFeed(){
-
-        // feed navigation bar top
-        HBox feed_navigation_bar = new HBox();
-        Button previous_road = new Button("<--");
-        previous_road.setId("title");
-        Button next_road = new Button("-->");
-        next_road.setId("title");
-        Text road_number_text = new Text("Road X");
-        road_number_text.setId("title");
-
-        Region fill_top = new Region();
-        fill_top.setPrefWidth(90);
-        feed_navigation_bar.setSpacing(30);
-        feed_navigation_bar.getChildren().addAll(fill_top, previous_road, road_number_text, next_road);
-
-
-        // feed navigation bottom
-        HBox feed_timerange_bar = new HBox();
-
-        Button previus_timerange = new Button("<--");
-        previus_timerange.setId("title");
-        Button next_timerange = new Button("-->");
-        next_timerange.setId("title");
-        Text current_time_text = new Text("2h");
-        current_time_text.setId("title");
-
-        Region fill_bottom = new Region();
-        fill_bottom.setPrefWidth(100);
-        feed_timerange_bar.setSpacing(30);
-        feed_timerange_bar.getChildren().addAll(fill_bottom,previus_timerange, current_time_text, next_timerange);
-
-        feed_window.setId("feed-window");
-        feed_window.setAlignment(Pos.TOP_RIGHT);
-        feed_window.getChildren().addAll(feed_navigation_bar, taskFeed.getElement(), feed_timerange_bar);
-
+        taskFeed = new Feed(task_list);
     }
 
     private void createContent()  {
@@ -208,32 +285,16 @@ public class TrafficPageScene extends Scene{
 
         // vertical layout
 
-        rootVerticalContainer.setMinWidth(1024);
-        rootVerticalContainer.setAlignment(Pos.CENTER);
-        rootVerticalContainer.setId("background");
-
-        Button backButton = new Button("<- back to menu");
-        backButton.setPrefSize(120,40);
-        backButton.setOnAction(event->backToMenuClickHandle());
-        rootVerticalContainer.getChildren().add(backButton);
-
-        filler = new Region();
-        filler.setPrefWidth(50);
-
-
-
-        mainContent.getChildren().addAll(graph, feed_window);
-
-        horizontalrootElementContainer.getChildren().addAll(mainContent,filler, sidepanel);
-        
-        rootVerticalContainer.getChildren().addAll(horizontalrootElementContainer);
-
-
-        // Possible nullPointerException throwing from .toExternalForm()
-        this.getStylesheets().add(TrafficPageScene.class.getResource("/stylesheet.css").toExternalForm());
 
     }
 
+
+    private void refreshWithNewData(){
+
+        makeNewChartViewer(coords,wantedData);
+        populateFeed();
+        feed_window.getChildren().set(1,taskFeed.getElement());
+    }
 
 
     /**
@@ -244,43 +305,11 @@ public class TrafficPageScene extends Scene{
         controller.fromAnyPageToMenu();
     }
 
-    // For test usage of RoadDataProvider's and WeatherDataProvider's functions
-    /*private static void testCase() throws IOException {
-        ArrayList<String> places = new ArrayList<>();
-        places.add("Tampere");
-        places.add("Rovaniemi");
-
-        ArrayList<String> params = new ArrayList<>();
-        params.add("temperature");
-        params.add("windspeedms");
-        // params.add("winddirection");
-        // params.add("pressure");
-        // params.add("humidity");
-        // params.add("windgust");
-        // params.add("totalcloudcover");
-
-        String stime = Utility.dateFormatter(2022, 10, 12, 0, 0); // starttime
-        String etime = Utility.dateFormatter(2022, 10, 13, 0, 0); // endtime
-
-        double minLong = 24;
-        double minLat = 61;
-        double maxLong = 24.5;
-        double maxLat = 61.4;
-        ArrayList<Double> coord = new ArrayList<>();
-        coord.add(minLong);
-        coord.add(minLat);
-        coord.add(maxLong);
-        coord.add(maxLat);
-
-        ArrayList<String> taskIds = new ArrayList<>();
-        taskIds.add("ROAD_STATE_CHECKING");
-        taskIds.add("DITCHING");
-
-        RoadDataProvider.getMaintenanceData(coord, taskIds, stime, etime);
-        RoadDataProvider.getTrafficMessages(1, "TRAFFIC_ANNOUNCEMENT");
-        WeatherDataProvider.weatherURLCreator(places, new ArrayList<Double>(), params, stime, etime);
+    private void handleSearchButtonClick(){
+        getDataFromApi();
+        refreshWithNewData();
     }
-*/
-     
+
+
 
 }
